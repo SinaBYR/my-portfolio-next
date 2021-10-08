024865728266329e3cd38b8702966b818a775f13
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-10-08 12:00:09 +0330

    Add showcase text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Navbar } from "./components/Navbar/Navbar";
+import { Showcase } from "./components/Showcase/Showcase";
 
 const App = () => {
     return (
             <div>
                 <Navbar />
+                <Showcase />
             </div>
     )
 }
