80ca72c10bc04172f9a118ebbe71f7929ee661f2
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-10-15 17:53:10 +0330

    Install Axios + update manifest.json file

--- src/App.js
+++ src/App.js
@@ -5,9 +5,22 @@ import { Contact } from "./components/Contact/Contact";
 import { Footer } from "./components/Footer/Footer";
 import { useContext } from "react/cjs/react.development";
 import { ThemeContext } from "./dark-mode-future/theme-context";
+import { useEffect } from "react";
+import axios from './axios/axios';
 
 const App = () => {
     const dark = useContext(ThemeContext)
+
+    const get = async () => {
+        const response = await axios.get('/projects')
+        const data = response.data;
+        console.log(data)
+    }
+
+    useEffect(() => {
+        get()
+    })
+
     return (
         <div style={{color: dark ? '#f7f7f7' : '#1e262c'}}>
             <Navbar />
