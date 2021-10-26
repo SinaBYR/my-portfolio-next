bdced8e19d17f2caf9ab895514ba2107426d490d
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-10-26 10:42:31 +0330

    Make them ready for prod

--- src/components/Dashboard/Create/Create.js
+++ src/components/Dashboard/Create/Create.js
@@ -1,11 +1,29 @@
+import axios from '../../../axios/axios';
+import { useState } from 'react';
 import { Form } from '../Form/Form';
 import classes from './Create.module.css';
 
-const Create = ({ create, loading }) => {
-
+const Create = () => {
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const createProject = async payload => {
+        setLoading(true)
+        setError(null)
+        try {
+            const response = await axios.post('/projects', payload)
+            const data = await response.data
+            console.log(response)
+            console.log(data)
+            setLoading(false)
+        } catch(err) {
+            console.log(err)
+            setError(err)
+            setLoading(false)
+        }
+    }
     return (
         <div className={classes.Create}>
-            <Form submit={create} loading={loading}/>
+            <Form submit={createProject} loading={loading} error={error}/>
         </div>
     )
 }
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,32 +1,17 @@
-import { useState } from 'react';
 import classes from './Dashboard.module.css';
-import { Form } from './Form/Form';
-import axios from '../../axios/axios';
 import { Header } from './Header/Header';
 import { Projects } from './Projects/Projects';
 import { Route, Switch } from 'react-router';
+import { Create } from './Create/Create';
 
 const Dashboard = () => {
-    const [loading, setLoading] = useState(false)
-    const create = async formData => {
-        // const form = new FormData()
-        // form.append('title', formData.title)
-        // form.append('desc', formData.desc)
-        // form.append('demo', formData.demo)
-        // form.append('code', formData.code)
-        // form.append('tech', formData.tech)
-        // form.append('preview', formData.preview)
-        // const response = await axios.post('/projects', form)
-        // const data = await response.data;
-        console.log(formData)
-    }
-
     return (
         <div className={classes.Dashboard}>
             <div className={classes.DashboardWrapper}>
                 <Header />
                 <Switch>
-                    <Route path="/create" render={() => <Form create={create} loading={loading}/>}/>
+                    {/* <Route path="/create" render={() => <Form create={create} loading={loading}/>}/> */}
+                    <Route path="/create" component={Create}/>
                     <Route path="/" component={Projects}/>
                 </Switch>
             </div>
