30a6847a4722f093405af0f3be570ba07b32cc40
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-10-29 21:22:32 +0330

    Make changes to isValid handler of EditForm + Change the way preview image is displayed

---
+++ src/components/Dashboard/Forms/EditForm/EditForm.js
@@ -0,0 +1,180 @@
+import classes from './EditForm.module.css';
+import { PrimaryButton, WarningButton } from '../../../Utilities';
+import { Input } from '..';
+import { ScaleLoader } from 'react-spinners'
+import { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
+import { setValues, isValid } from './handlers';
+
+const EditForm = ({ submit, preData, loading, error}) => {
+    const [formData,setFormData] = useState({
+        title: '',
+        preview: null,
+        demo: '',
+        code: '',
+        description: '',
+        tech: ['']
+    })
+    const addInputHandler = () => {
+        // I add an empty string to the formData.tech to turn newly added tech input into a controlled input.
+        const newTechArray = [...formData.tech]
+        newTechArray.push('')
+        setFormData({
+            ...formData,
+            tech: newTechArray
+        })
+    }
+    const deleteInputHandler = () => {
+        if(formData.tech.length < 2) {
+            return
+        }
+
+        const newTechArray = [...formData.tech]
+        newTechArray.pop()
+
+        setFormData({
+            ...formData,
+            tech: newTechArray
+        })
+    }
+    const onChangeHandler = e => {
+        setFormData(setValues(formData, e))
+    }
+    const onSubmitHandler = e => {
+        e.preventDefault()
+
+        const data = new FormData()
+        Object.keys(formData).forEach(field => {
+            if(field === 'tech') {
+                return formData[field].forEach(value => {
+                    data.append('tech[]', value)
+                })
+            }
+
+            data.append(field, formData[field])
+        })
+
+        if(!isValid(formData)) {
+            return
+        }
+
+        console.log(formData)
+        // submit(data)
+    }
+
+    useEffect(() => {
+        if(!Object.keys(preData).length) {
+            return
+        }
+        const newFormData = {...formData}
+        Object.keys(preData).forEach(field => {
+            if(field === '_id' || field === 'preview') {
+                return
+            }
+
+            newFormData[field] = preData[field]
+        })
+        setFormData(newFormData)
+    }, [preData])
+
+    let techElements = formData.tech.map((_, i) => {
+        return (
+            <Input
+                key={i}
+                type="text"
+                label={"Tech " + (i + 1)}
+                name="tech"
+                id={i}
+                value={formData.tech[i]}
+                onChange={onChangeHandler}
+            />
+        )
+    })
+
+    return (
+        <div className={classes.EditForm}>
+            <form className={classes.Form} onSubmit={onSubmitHandler}>
+                {error && <div className={classes.Error}>{error.message}</div>}
+                <Input
+                    key="title"
+                    type="text"
+                    label="Title"
+                    name="title"
+                    id="title"
+                    value={formData.title}
+                    onChange={onChangeHandler}
+                    />
+                <Input
+                    key="demo"
+                    type="text"
+                    label="Demo Link"
+                    id="demo"
+                    name="demo"
+                    value={formData.demo}
+                    onChange={onChangeHandler}
+                    />
+                <Input
+                    key="code"
+                    type="text"
+                    label="Code Link"
+                    id="code"
+                    name="code"
+                    value={formData.code}
+                    onChange={onChangeHandler}
+                    />
+                <Input
+                    key="description"
+                    type="textarea"
+                    label="Description"
+                    name="description"
+                    id="description"
+                    value={formData.description}
+                    onChange={onChangeHandler}
+                    />
+                <Input
+                    key="preview"
+                    type="file"
+                    label="Preview"
+                    name="preview"
+                    id="preview"
+                    accept="image/png, image/jpeg"
+                    onChange={onChangeHandler}
+                    />
+                <div className={classes.Previews}>
+                    {
+                        formData.preview
+                        // if an image file is chosen, then that image will be displayed.
+                        ?
+                        <img
+                            className={classes.Preview}
+                            src={URL.createObjectURL(formData.preview)}
+                            alt="upload-preview" />
+                        :
+                        // if an image file is NOT chosen, then image from database will be displayed.
+                        <img
+                            className={classes.Preview}
+                            src={preData.preview}
+                            alt="upload-preview" />
+                    }
+                </div>
+                <div className={classes.Technologies}>{techElements}</div>
+                <div className={classes.ButtonsWrapper}>
+                    <PrimaryButton type="button" onClick={addInputHandler}>Add Tech</PrimaryButton>
+                    <PrimaryButton type="button" onClick={deleteInputHandler}>Delete Tech</PrimaryButton>
+                    <PrimaryButton type="submit" onClick={onSubmitHandler}>
+                        {
+                            loading
+                            ?
+                            <ScaleLoader color="#eeeeee" height="10px" radius="2px"/>
+                            :
+                            'Update'
+                        }
+                    </PrimaryButton>
+                    <WarningButton>Delete</WarningButton>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export { EditForm }
