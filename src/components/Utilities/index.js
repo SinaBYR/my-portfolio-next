6eb411b543300d1aab6e96079ac5c55a33c38803
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-10-29 22:11:51 +0330

    Remove Spinner export

--- src/components/Utilities/index.js
+++ src/components/Utilities/index.js
@@ -3,7 +3,6 @@ export { SecondaryButton } from "./Buttons/SecondaryButton/SecondaryButton";
 export { WarningButton } from './Buttons/WarningButton/WarningButton';
 export { PrimaryLink } from "./Links/PrimaryLink/PrimaryLink";
 export { SecondaryLink } from "./Links/SecondaryLink/SecondaryLink";
-export { Spinner } from './Spinner/Spinner';
 export { Logo } from './Logo/Logo';
 export { Burger } from './Burger/Burger';
 export { ErrorMessage } from './ErrorMessage/ErrorMessage';
