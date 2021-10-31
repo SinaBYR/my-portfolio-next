f5132e5e348500eea3cc371f50468f8aed3bcc37
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-10-31 20:55:33 +0330

    Set up fetching projects

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,11 +1,42 @@
 import classes from './Work.module.css';
+import { useState, useContext, useEffect } from 'react';
 import { Project } from './Project/Project';
-import { useContext } from 'react/cjs/react.development';
 import { ThemeContext } from '../../dark-mode-future/theme-context';
 import { colors } from '../../color-palette/color-palette';
+import { axios } from '../../axios/axios';
+import { ScaleLoader } from 'react-spinners';
+import { ErrorMessage } from '../Utilities';
 
 const Work = () => {
     const dark = useContext(ThemeContext)
+    const [projects, setProjects] = useState([])
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    const fetchProjects = async () => {
+        setError(null)
+        setLoading(true)
+        try {
+            const response = await axios.get('/projects')
+            const data = await response.data
+            setProjects(data)
+            setLoading(false)
+        } catch(err) {
+            console.log(err)
+            setError(err)
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchProjects()
+    }, [])
+
+    let display = projects.map(({ _id, title, code, demo, description, tech, preview}) => {
+        return (
+            <Project key={_id} title={title} code={code} demo={demo} description={description} tech={tech} preview={preview}/>
+        )
+    })
 
     return (
         <main
@@ -14,8 +45,17 @@ const Work = () => {
             <div className={classes.WorkWrapper}>
                 <h2>My Work</h2>
                 <div className={classes.Projects}>
-                    <Project />
-                    <Project />
+                    {
+                        error
+                        ?
+                        <ErrorMessage>{error.message}</ErrorMessage>
+                        :
+                            loading
+                            ?
+                            <ScaleLoader color="#f7f7f7"/>
+                            :
+                            display
+                    }
                 </div>
             </div>
         </main>
