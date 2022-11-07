dcf9b682e97e5c2f9b133a051eac3caf99911ea5
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-07 23:50:52 +0330

    Add ThemeProvider to custom _app.js file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
+import '../../styles/index.css';
 import Layout from "../components/layout/layout";
+import { ThemeProvider } from "../dark-mode-future/theme-context";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {...pageProps}/>
+      <ThemeProvider>
+        <Component {...pageProps}/>
+      </ThemeProvider>
     </Layout>
   )
 }
