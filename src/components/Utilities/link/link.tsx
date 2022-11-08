a4ccf7a5435dc503768183ad66191a4041a46a91
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-08 13:46:04 +0330

    Make custom props optional, also provide default value

--- src/components/Utilities/link/link.tsx
+++ src/components/Utilities/link/link.tsx
@@ -2,13 +2,12 @@ import NextLink, { LinkProps } from "next/link";
 import styles from './link.module.css';
 
 type Props = {
-  external: boolean;
-  model: 'primary' | 'secondary'
+  external?: boolean;
+  mode?: 'primary' | 'secondary'
 } & LinkProps & React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
-
-export const Link: React.FC<Props> = ({ external, model, children, ...rest }) => {
-  const classes = [styles.link, styles[model]]
+export const Link: React.FC<Props> = ({ mode = 'primary', external = false, children, ...rest }) => {
+  const classes = [styles.link, styles[mode]];
   if(external) {
     return <a className={classes.join(' ')} {...rest}>{children}</a>
   }
