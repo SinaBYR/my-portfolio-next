7205b4b8941e63ccf46f41135eb5794bb24bfd00
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-08 13:43:53 +0330

    Remove Anchors util components

--- src/components/Utilities/index.js
+++ src/components/Utilities/index.js
@@ -1,7 +1,5 @@
 export { Button } from "./button/button";
 export { Link } from "./link/link";
-export { PrimaryAnchor } from "./Anchors/PrimaryAnchor/PrimaryAnchor";
-export { SecondaryAnchor } from "./Anchors/SecondaryAnchor/SecondaryAnchor";
 export { Logo } from './Logo/Logo';
 export { Burger } from './Burger/Burger';
 export { ErrorMessage } from './ErrorMessage/ErrorMessage';
