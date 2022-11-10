1eb8463115d2497313c42a44de52e1477d692820
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-11 01:00:51 +0330

    Update showcase content, and also update desktop css code

--- src/components/showcase/showcase.tsx
+++ src/components/showcase/showcase.tsx
@@ -4,11 +4,18 @@ export function Showcase() {
   return (
     <section className={classes.showcase} >
       <div className={classes.showcaseWrapper}>
-        <h2>Hello, and welcome to my portfolio.</h2>
-        <p>My name is Sina. I'm a front-end web developer and I make web applications.</p>
-        <p>Check out my work in the next section.</p>
-        {/* <p>Send me an email using form down below or message me in LinkedIn.</p> */}
-        <p>Send me an email using the form in the last section or message me in LinkedIn.</p>
+        <h2>
+          <span>Let's give</span>
+          <br />
+          <span>your business</span>
+          <br />
+          <span>wings to fly.</span>
+        </h2>
+        <p>
+          <span>I’m a Software Engineer who builds reliable, performant</span>
+          <br />
+          <span>and secure web-based applications.</span>
+        </p>
       </div>
     </section>
   )
