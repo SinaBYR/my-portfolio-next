1a702322bf11b8bcd664f7218d8b36904d0827d2
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-10 10:30:01 +0330

    Remove all dark-mode related code, and migrate to sass and typescript.

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,15 +1,12 @@
 import { Navbar } from '../navbar/navbar';
-import { Footer } from '../Footer/Footer';
-import { useContext } from 'react';
-import { ThemeContext } from '../../dark-mode-future/theme-context';
+import { Footer } from '../footer/footer';
 
 export default function Layout({ children }) {
-  const darkMode = useContext(ThemeContext);
   return (
-    <div style={{color: darkMode ? '#f7f7f7' : '#1e262c'}}>
+    <>
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </div>
+    </>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import '../../styles/index.css';
 import Layout from "../components/layout/layout";
-import { ThemeProvider } from "../dark-mode-future/theme-context";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-      <Layout>
-          <Component {...pageProps}/>
-      </Layout>
-    </ThemeProvider>
+    <Layout>
+        <Component {...pageProps}/>
+    </Layout>
   )
 }
