4e23aae506b9a4cd381007709c0b5e593aa6b628
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-12 19:00:36 +0330

    Set higher height, new background-color and new color property for navbar, and add a wrapper for Sina-Beyraghdar logo

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,14 +2,14 @@ import classes from './navbar.module.scss';
 import { useState } from 'react';
 import { NavLinks } from './navLinks/navLinks';
 import { BsDownload } from 'react-icons/bs';
-import { Burger, Link } from '../Utilities';
+import { Burger, Link, Logo } from '../Utilities';
 import { Menu } from './menu/menu';
 
 export function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   function menuClickHandler() {
-    setIsMenuOpen(!isMenuOpen);
+    setIsMenuOpen(state => !state);
   }
 
   return (
@@ -17,15 +17,19 @@ export function Navbar() {
       <nav className={classes.navbarWrapper}>
         <Burger open={isMenuOpen} click={menuClickHandler}/>
         <Menu open={isMenuOpen}/>
-        <h1 className={classes.logo}>Sina Beyraghdar</h1>
-        <ul className={classes.links}>
-          {/* <NavLinks /> */}
-        </ul>
-        <Link
+        {/* <h1 className={classes.logo}>Sina Beyraghdar</h1> */}
+        <div className={classes.logoWrapper}>
+          {/* <Image src={LogoImageSrc} layout="fill"/> */}
+          <Logo />
+        </div>
+        {/* <ul className={classes.links}>
+          <NavLinks />
+        </ul> */}
+        {/* <Link
           mode="secondary"
           href="https://drive.google.com/file/d/1HJoJi-s_4c22NDENLhX4bPYliS7D6GJL/view?usp=sharing"
           target="_blank"
-          rel="noopener noreferrer">Resume</Link>
+          rel="noopener noreferrer">Resume</Link> */}
       </nav>
     </header>
   )
