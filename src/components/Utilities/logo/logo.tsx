831567ea626511c6969ca7a41dff4a8a96efc09b
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-12 18:55:04 +0330

    Create Sina-Beyraghdar logo with html/css

---
+++ src/components/Utilities/logo/logo.tsx
@@ -0,0 +1,16 @@
+import classes from './logo.module.scss';
+
+export function Logo() {
+  return (
+    <div className={classes.wrapper}>
+      <div>
+        <h2>S</h2>
+        <span>INA</span>
+      </div>
+      <div>
+        <h2>B</h2>
+        <span>EYRAGHDAR</span>
+      </div>
+    </div>
+  )
+}
