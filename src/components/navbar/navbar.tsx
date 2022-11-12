6ea852935efa35f55ade909c7aa4d290f19f0522
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-12 20:24:47 +0330

    Listen to window's scroll event, then shrink or unshrink navbar height, also set different values for Logo component's fontSize.

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,5 +1,5 @@
 import classes from './navbar.module.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { NavLinks } from './navLinks/navLinks';
 import { BsDownload } from 'react-icons/bs';
 import { Burger, Link, Logo } from '../Utilities';
@@ -7,21 +7,50 @@ import { Menu } from './menu/menu';
 
 export function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // isShrunk is used to shrink or unshrink the navbar. It also determines <Logo /> component's fontSize prop.
+  const [isShrunk, setIsShrunk] = useState(false);
 
   function menuClickHandler() {
     setIsMenuOpen(state => !state);
   }
 
+  function onScrollHandler() {
+    setIsShrunk((isShrunk) => {
+      if(
+        !isShrunk &&
+        (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40)
+      ) {
+        return true;
+      }
+
+      if(
+        isShrunk &&
+        (document.body.scrollTop < 5 && document.documentElement.scrollTop < 5)
+      ) {
+        return false;
+      }
+
+      return isShrunk;
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScrollHandler);
+    return () => window.removeEventListener('scroll', onScrollHandler);
+  }, [])
+
   return (
-    <header className={classes.header}>
+    <header className={[classes.header, isShrunk ? classes.shrunk : null].join(' ')}>
       <nav className={classes.navbar}>
         <Burger open={isMenuOpen} click={menuClickHandler}/>
         <Menu open={isMenuOpen}/>
         {/* <h1 className={classes.logo}>Sina Beyraghdar</h1> */}
+        {/* {isShrunk ? 'true' : 'false'} */}
         <div className={classes.logoWrapper}>
           {/* <Image src={LogoImageSrc} layout="fill"/> */}
-          <Logo />
+          <Logo fontSize={isShrunk ? '1.2rem' : '1.6rem'}/>
         </div>
+
         {/* <ul className={classes.links}>
           <NavLinks />
         </ul> */}
