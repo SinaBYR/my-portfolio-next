d91a9ff3474d701344a293f8646e9138a53f0b7c
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-22 12:27:27 +0330

    Finish footer section.

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,36 @@
+import { BsGithub, BsLinkedin, BsTwitter } from 'react-icons/bs';
+import { Link, Logo } from '../Utilities';
 import classes from './footer.module.scss';
 
 export function Footer() {
   return (
     <footer className={classes.footer}>
-      <p>Developed by Sina Beyraghdar © 2021</p>
-      <p className={classes.photographer}>Pian delle Betulle, Italy. Photo by <a href="https://unsplash.com/@asoggetti?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Alessio Soggetti</a> on <a href="https://unsplash.com/@asoggetti?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash.</a>
-      </p>
+      <div className={classes.wrapper}>
+        <div>
+          <div className={classes.logoWrapper}>
+            <Logo fontSize="16px"/>
+          </div>
+          <p>Developed by Sina Beyraghdar © 2021</p>
+        </div>
+        <div>
+          <div className={classes.linksWrapper}>
+            <Link variant="simple" href="/projects">Projects</Link>
+            <Link variant="simple" href="/projects">About me</Link>
+            <Link variant="simple" href="/projects">Panel</Link>
+          </div>
+          <div className={classes.socials}>
+            <a href="https://www.github.com/SinaBYR" target="_blank" rel="noopener noreferrer">
+              <BsGithub />
+            </a>
+            <a href="https://www.linkedin.com/in/sina-beyraghdar" target="_blank" rel="noopener noreferrer">
+              <BsLinkedin />
+            </a>
+            <a href="https://www.twitter.com/sinabyr" target="_blank" rel="noopener noreferrer">
+              <BsTwitter />
+            </a>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
