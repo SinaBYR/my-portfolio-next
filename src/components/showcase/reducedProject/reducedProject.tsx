f46edf2a7816e805966e51fd60d4db0bca8ef67e
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-23 10:16:23 +0330

    Create reducedProject just to display a brief overview of latest projects.

---
+++ src/components/showcase/reducedProject/reducedProject.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import classes from './reducedProject.module.scss';
+import img from '../../../../public/img/alessio-soggetti-gdE-5Oui1Y0-unsplash.webp';
+import { Link } from '../../Utilities';
+
+interface Props {
+  id: string;
+  title: string;
+  description: string;
+  preview: string;
+  tech: string[];
+}
+
+export function ReducedProject({
+  id,
+  title,
+  description,
+  preview,
+  tech
+}: Props) {
+  return (
+    <div className={classes.wrapper} key={id}>
+      <div className={classes.preview}>
+        <Image src={preview} layout="fill"/>
+        <div className={classes.title}>{title}</div>
+      </div>
+      <div className={classes.content}>
+        <div className={classes.tech}>
+          {tech.map(val => <span>{val}</span>)}
+        </div>
+        <p>{description}</p>
+          <Link variant="secondary" href={"/projects/" + id}>Read more</Link>
+      </div>
+    </div>
+  )
+}
