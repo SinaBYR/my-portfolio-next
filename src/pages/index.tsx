b7611137e0480c992c70767353b8ebe8113273c2
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-23 10:17:14 +0330

    Finish Showcase component UI design to display latest done projects.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
 import { Landing } from '../components/landing/landing';
-import { Work } from '../components/work/work';
-import { Contact } from '../components/Contact/Contact';
+import { Showcase } from '../components/showcase/showcase';
+import { Contact } from '../components/contact/contact';
 import { Skills } from '../components/skills/skills';
+import { db } from '../db/db';
 
-export default function Home() {
+// export async function getServerSideProps() {
+//   try {
+//     const rows = await db.pool.query('select * from books;');
+//     // const rows = await conn.query('select 1 as val;');
+//     console.log(rows);
+//     return {
+//       props: {
+//         rows
+//       }
+//     }
+//   } catch(err) {
+//     console.log(err);
+//   }
+// }
+
+export default function Home({ rows }: {rows: string}) {
   return (
     <>
       <Landing />
       <Skills />
-      <Work />
+      <Showcase />
       <Contact />
     </>
   )
