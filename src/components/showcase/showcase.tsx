44c6bd3c88caf112bbe9b688f06163f4e1ea7cb5
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-25 19:17:46 +0330

    Fetch two reduced projects from database, along with their technology list, and display in Showcase component.

--- src/components/showcase/showcase.tsx
+++ src/components/showcase/showcase.tsx
@@ -1,86 +1,31 @@
 import classes from './showcase.module.scss';
-import { useState, useEffect } from 'react';
-import { Project } from '../projects/project/project';
-import { axios } from '../../axios/axios';
-// import { ScaleLoader } from 'react-spinners';
-import { ErrorMessage, Link } from '../Utilities';
+import { Link } from '../Utilities';
 import { ReducedProject } from './reducedProject/reducedProject';
+import { ReducedProjectType, Technology } from '../../types/types';
 
-export function Showcase() {
-  const [projects, setProjects] = useState([])
-  const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(false)
+interface Props {
+  projects: ReducedProjectType[]
+  technologies: Technology[]
+}
 
-  const fetchProjects = async () => {
-    setError(null)
-    setLoading(true)
-    try {
-        const response = await axios.get('/projects')
-        const data: any = await response.data
-        setProjects(data)
-        setLoading(false)
-    } catch(err) {
-        console.log(err)
-        setError(err)
-        setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    fetchProjects()
-  }, [])
-
-  let display = projects?.map(({
-    _id,
-    title,
-    code,
-    demo,
-    description,
-    tech,
-    preview
-  }) => {
-    return (
-      <Project
-        key={_id}
-        title={title}
-        code={code}
-        demo={demo}
-        description={description}
-        tech={tech}
-        preview={preview}
-      />
-    )
-  })
-
+export function Showcase({ projects, technologies }: Props) {
   return (
     <main className={classes.showcase}>
       <div className={classes.showcaseWrapper}>
         <h2>Latest Projects</h2>
         <div className={classes.projects}>
-          {
-            // error
-            // ?
-            // <ErrorMessage>{error}</ErrorMessage>
-            // :
-            //   loading
-            //   ?
-            //   <div style={{textAlign: 'center'}}>
-            //       {/* <ScaleLoader color={!dark ? colors.dark.secondary : colors.light.primary}/> */}
-            //   </div>
-            //   :
-            //   display
-          }
-
-          {/* <ReducedProject
-            description="description"
-            preview="preview"
-            tech={["tech1", "tech2"]}
-            title="title"/> */}
-          {/* <ReducedProject
-            description="description"
-            preview="preview"
-            tech={["tech1", "tech2"]}
-            title="title"/> */}
+          {projects.map(p => {
+            const techArray = technologies.filter(t => t.p_id === p.id).map(t => t.name);
+            return (
+              <ReducedProject 
+                id={p.id}
+                title={p.title}
+                description={p.description}
+                preview="/img/babel.png"
+                key={p.id}
+                tech={techArray}/>
+            )
+          })}
         </div>
         <div className={classes.linkWrapper}>
           <Link href="/projects">See more projects</Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,28 +3,48 @@ import { Showcase } from '../components/showcase/showcase';
 import { Contact } from '../components/contact/contact';
 import { Skills } from '../components/skills/skills';
 import { db } from '../db/db';
+import { ReducedProjectType, Technology } from '../types/types';
 
-// export async function getServerSideProps() {
-//   try {
-//     const rows = await db.pool.query('select * from books;');
-//     // const rows = await conn.query('select 1 as val;');
-//     console.log(rows);
-//     return {
-//       props: {
-//         rows
-//       }
-//     }
-//   } catch(err) {
-//     console.log(err);
-//   }
-// }
+export async function getServerSideProps() {
+  try {
+    const projects: ReducedProjectType[] = await db.pool.query(`
+      select id, title, description, created_at
+      from project
+      limit 2;
+    `);
+    const p_ids = projects.map(p => `'${p.id}'`).join(',');
+    const technologies: Technology[] = await db.pool.query(`
+      select *
+      from technology
+      where p_id in (${p_ids});
+    `);
 
-export default function Home({ rows }: {rows: string}) {
+    console.log(technologies);
+    return {
+      props: {
+        projects: JSON.parse(JSON.stringify(projects)),
+        technologies: technologies
+      }
+    }
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+export default function Home(
+  {
+    projects,
+    technologies
+  }:{
+    projects: ReducedProjectType[],
+    technologies: Technology[]
+  }
+) {
   return (
     <>
       <Landing />
       <Skills />
-      <Showcase />
+      <Showcase projects={projects} technologies={technologies}/>
       <Contact />
     </>
   )
