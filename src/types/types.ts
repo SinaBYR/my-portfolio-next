453aecdd850630508b7148b8a64bbe97a089e3d3
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-25 18:44:28 +0330

    Add ReducedProjectType and Technology interfaces.

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export interface ReducedProjectType {
+  id: string;
+  title: string;
+  description: string;
+  preview: string;
+}
+
+export interface Technology {
+  id: string;
+  name: string;
+  p_id: string;
+}
