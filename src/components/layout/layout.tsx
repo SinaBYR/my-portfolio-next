95d1d512b20d8f8d0f74efba15c5b2a1a80321b7
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-26 21:18:52 +0330

    Remove margin top from Landing component, and add it to main tag inside layout. This way, the navbar does not cover page content in every page.

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,11 +1,12 @@
 import { Navbar } from '../navbar/navbar';
 import { Footer } from '../footer/footer';
 
+// marginTop value is equal to height of navbar.
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <main style={{marginTop: '100px'}}>{children}</main>
       <Footer />
     </>
   )
