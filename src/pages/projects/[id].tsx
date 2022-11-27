b49c3a7c99daa624844da23ee4f39dc1b9451222
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-27 18:28:12 +0330

    Create FullProjectPage page (dynamic route), and showcase one specific project in details.

---
+++ src/pages/projects/[id].tsx
@@ -0,0 +1,40 @@
+import { GetServerSideProps } from "next";
+import { Project } from "../../components/projects/project/project";
+import { db } from "../../db/db";
+import { getProject } from "../../lib/projects";
+import { FullProject, Technology } from "../../types/types";
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { project, technologies } = await getProject(params.id as string)
+  return {
+    props: {
+      project,
+      technologies
+    }
+  }
+}
+
+export default function FullProjectPage({ project, technologies }: { project: FullProject; technologies: Technology[] }) {
+  const {
+    id,
+    title,
+    description,
+    preview,
+    code_link,
+    demo_link,
+    created_at,
+    edited_at
+  } = project;
+  return (
+    <Project
+      key={id}
+      title={title}
+      description={description}
+      preview={preview}
+      code_link={code_link}
+      demo_link={demo_link}
+      tech={technologies}
+      created_at={created_at}
+      edited_at={edited_at}/>
+  )
+}
