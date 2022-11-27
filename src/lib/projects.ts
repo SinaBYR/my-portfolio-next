544fa96fb118f125c92c856f0b7d8d9d2db20fd6
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-27 13:33:46 +0330

    Add getProject function, which is used to fetch data of a single project.

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,5 +1,5 @@
 import { db } from "../db/db";
-import { ReducedProjectType, Technology } from "../types/types";
+import { FullProject, ReducedProjectType, Technology } from "../types/types";
 
 // This util function is used to retrieve reduced projects for
 // showcase section of index page.
@@ -35,6 +35,33 @@ export async function getReducedProjects(limit?: number) {
     return {
       projects: JSON.parse(JSON.stringify(projects)),
       technologies: technologies
+    }
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+// This util function retrieves a single project data.
+// It's used in pages/projects/[id].tsx component to
+// fetch individual project.
+// id: required => used to fetch a single matching project data.
+export async function getProject(id: string) {
+  try {
+    const project: FullProject[] = await db.pool.query(`
+      select *
+      from project
+      where id = '${id}';
+    `);
+
+    const technologies: Technology[] = await db.pool.query(`
+      select *
+      from technology
+      where p_id = '${project[0].id}';
+    `);
+
+    return {
+      project: JSON.parse(JSON.stringify(project[0])),
+      technologies
     }
   } catch(err) {
     console.error(err);
