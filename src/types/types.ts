c8cd493a4b20b342c23052ce85810cd65d11b6f2
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-27 13:30:15 +0330

    Add FullProject interface.

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,14 @@
+export interface FullProject {
+  id: string;
+  title: string;
+  description: string;
+  code_link: string;
+  demo_link: string;
+  preview: string[];
+  created_at: string;
+  edited_at: string;
+}
+
 export interface ReducedProjectType {
   id: string;
   title: string;
