9bd24ed88443362ae8b2140830604c54c3696815
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-11-29 21:07:40 +0330

    Add loading state, and display skeletons while fetching conributor's avatar.

--- src/components/projects/project/sidebar/contributors/contributors.tsx
+++ src/components/projects/project/sidebar/contributors/contributors.tsx
@@ -3,116 +3,120 @@ import Image from 'next/image';
 import { useEffect, useState } from "react";
 import { octokit } from "../../../../../gh/gh";
 import { MdMoreHoriz } from 'react-icons/md';
+import { ContributorsSkeleton } from '../../../../skeletons/contributors/contributors';
 
 interface Props {
   repo: string;
 }
 
 export function Contributors({ repo }: Props) {
-  const [contributors, setContributors] = useState<any[]>([
-    {
-      "login": "1",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 16
-    },
-    {
-      "login": "2",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 15
-    },
-    {
-      "login": "3",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 14
-    },
-    {
-      "login": "4",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 13
-    },
-    {
-      "login": "5",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 12
-    },
-    {
-      "login": "6",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 11
-    },
-    {
-      "login": "7",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 10
-    },
-    {
-      "login": "8",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 9
-    },
-    {
-      "login": "9",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 8
-    },
-    {
-      "login": "10",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 7
-    },
-    {
-      "login": "11",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 6
-    },    {
-      "login": "12",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 5
-    },
-    {
-      "login": "13",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 4
-    },
-    {
-      "login": "14",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 3
-    },
-    {
-      "login": "15",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 2
-    },
-    {
-      "login": "16",
-      "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
-      "html_url": "https://github.com/SinaBYR",
-      "contributions": 1
-    },
-  ]);
+  // const [contributors, setContributors] = useState<any[]>([
+  //   {
+  //     "login": "1",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 16
+  //   },
+  //   {
+  //     "login": "2",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 15
+  //   },
+  //   {
+  //     "login": "3",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 14
+  //   },
+  //   {
+  //     "login": "4",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 13
+  //   },
+  //   {
+  //     "login": "5",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 12
+  //   },
+  //   {
+  //     "login": "6",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 11
+  //   },
+  //   {
+  //     "login": "7",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 10
+  //   },
+  //   {
+  //     "login": "8",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 9
+  //   },
+  //   {
+  //     "login": "9",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 8
+  //   },
+  //   {
+  //     "login": "10",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 7
+  //   },
+  //   {
+  //     "login": "11",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 6
+  //   },    {
+  //     "login": "12",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 5
+  //   },
+  //   {
+  //     "login": "13",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 4
+  //   },
+  //   {
+  //     "login": "14",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 3
+  //   },
+  //   {
+  //     "login": "15",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 2
+  //   },
+  //   {
+  //     "login": "16",
+  //     "avatar_url": "https://avatars.githubusercontent.com/u/59318782?v=4",
+  //     "html_url": "https://github.com/SinaBYR",
+  //     "contributions": 1
+  //   },
+  // ]);
+  const [contributors, setContributors] = useState<any[]>([]);
   const [isContributorsExpanded, setIsContributorsExpanded] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     // return;
     if(!repo) return;
 
     async function fetchContributors() {
+      setLoading(true);
       try {
         const result = await octokit.request('GET /repos/{owner}/{repo}/contributors{?anon,per_page,page}', {
           owner: 'sinabyr',
@@ -124,6 +128,8 @@ export function Contributors({ repo }: Props) {
         setContributors([...result.data])
       } catch(err) {
         console.error(err);
+      } finally {
+        setLoading(false);
       }
     }
 
@@ -133,6 +139,9 @@ export function Contributors({ repo }: Props) {
   return (
     <div className={classes.contributors}>
       <h4>Contributors</h4>
+      {
+      loading ? <ContributorsSkeleton />
+      :
       <ul className={classes.avatars}>
         {contributors.map((c, i) => {
           return (
@@ -161,6 +170,7 @@ export function Contributors({ repo }: Props) {
           </li>
         }
       </ul>
+      }
     </div>
   )
 }
