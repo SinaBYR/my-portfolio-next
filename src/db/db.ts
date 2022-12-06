b4aa5bdefc5d1ab38d2cec5533cf980aad19d0de
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-12-06 10:33:47 +0330

    Use environment variables for initializing database instance.

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,10 +2,10 @@ import mariadb from 'mariadb';
 
 export const db = Object.freeze({
   pool: mariadb.createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'sina',
-    password: 'Sina13801111',
-    database: 'my_portfolio'
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
   })
 });
