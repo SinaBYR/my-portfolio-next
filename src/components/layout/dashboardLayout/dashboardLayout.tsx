f9fd8d664412300629b6144fa6e7fc589a127b29
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-12-10 23:44:05 +0330

    Create a layout for dashboard page, which contains mobile-header, mobile-links, desktop-sidebar, and a workspace for contents of different routes.

---
+++ src/components/layout/dashboardLayout/dashboardLayout.tsx
@@ -0,0 +1,26 @@
+import classes from './dashboardLayout.module.scss'
+import { useState } from 'react';
+import { Sidebar } from "../../dashboard/sidebar/sidebar";
+import { MobileHeader } from '../../dashboard/mobileHeader/mobileHeader';
+import { SidebarLinks } from '../../dashboard/sidebarLinks/sidebarLinks';
+
+export default function DashboardLayout({ children }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function burgerClickHandler() {
+    setIsMenuOpen(state => !state);
+  }
+
+  return (
+    <main className={classes.dashboard}>
+      <div className={classes.dashboardWrapper}>
+        <MobileHeader isMenuOpen={isMenuOpen} onClickBurger={burgerClickHandler}/>
+        <ul className={[classes.menu, isMenuOpen ? classes.open : null].join(' ')}>
+          <SidebarLinks />
+        </ul>
+        <Sidebar />
+        <section className={classes.workspace}>{children}</section>
+      </div>
+    </main>
+  )
+}
