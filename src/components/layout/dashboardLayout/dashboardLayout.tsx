8e672e3d557f2a3b18b42674f5d8ee8e2bbc3c5b
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-12-11 20:43:36 +0330

    Redirect unauthenticated user to login page by calling useUser custom hook inside DashboardLayout.

--- src/components/layout/dashboardLayout/dashboardLayout.tsx
+++ src/components/layout/dashboardLayout/dashboardLayout.tsx
@@ -3,8 +3,15 @@ import { useState } from 'react';
 import { Sidebar } from "../../dashboard/sidebar/sidebar";
 import { MobileHeader } from '../../dashboard/mobileHeader/mobileHeader';
 import { SidebarLinks } from '../../dashboard/sidebarLinks/sidebarLinks';
+import { useUser } from '../../../lib/useUser';
 
 export default function DashboardLayout({ children }) {
+  // Because this component is shared between every dashboard component,
+  // then it'll redirect if user.isLoggedIn property returned by
+  // useUser is set to false.
+  useUser({
+    redirectTo: '/login'
+  });
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   function burgerClickHandler() {
