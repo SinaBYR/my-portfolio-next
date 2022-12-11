acfc571c302d9a2a8142b001ec595e62cb5b7798
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-12-11 19:47:00 +0330

    Create api route for dashboard projects section, which is protected behind user authentication.

---
+++ src/pages/api/projects.ts
@@ -0,0 +1,32 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../db/db";
+import { sessionOptions } from "../../lib/session";
+
+export default withIronSessionApiRoute(handler, sessionOptions)
+
+interface ProjectType {
+  id: string;
+  title: string;
+  created_at: string;
+  last_edited_at: string;
+}
+
+async function handler( req: NextApiRequest, res: NextApiResponse ) {
+  if(!req.session.userId) {
+    res.status(401).send('Unauthorized access.');
+  }
+
+  const query = `
+    select id, title, created_at, last_edited_at
+    from project
+    order by created_at desc;
+  `;
+
+  try {
+    const projects: ProjectType[] = await db.pool.query(query);
+    res.json(projects);
+  } catch(err) {
+    res.status(500).send(err);
+  }
+}
