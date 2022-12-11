cced206b524755ee33f9ce5b6fa28a676ce48106
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-12-11 15:07:33 +0330

    Add icons to sidebar links.

--- src/components/dashboard/sidebarLinks/sidebarLinks.tsx
+++ src/components/dashboard/sidebarLinks/sidebarLinks.tsx
@@ -1,22 +1,40 @@
 import { Link } from "../../utilities";
+import { FiSettings, FiInbox, FiLogOut } from 'react-icons/fi';
+import { GoProject } from 'react-icons/go';
+import { IoAnalytics } from 'react-icons/io5';
 
 export function SidebarLinks() {
   return (
     <>
       <li>
-        <Link variant="simple-alt" href="/dashboard">Analytics</Link>
+        <Link variant="simple-alt" href="/dashboard">
+          <IoAnalytics style={{marginRight: '16px'}}/>
+          Analytics
+        </Link>
       </li>
       <li>
-        <Link variant="simple-alt" href="/dashboard/inbox">Inbox</Link>
+        <Link variant="simple-alt" href="/dashboard/inbox">
+          <FiInbox style={{marginRight: '16px'}}/>
+          Inbox
+        </Link>
       </li>
       <li>
-        <Link variant="simple-alt" href="/dashboard/projects">Projects</Link>
+        <Link variant="simple-alt" href="/dashboard/projects">
+          <GoProject style={{marginRight: '16px'}}/>
+          Projects
+        </Link>
       </li>
       <li>
-        <Link variant="simple-alt" href="/dashboard/settings">Settings</Link>
+        <Link variant="simple-alt" href="/dashboard/settings">
+          <FiSettings style={{marginRight: '16px'}}/>
+          Settings
+        </Link>
       </li>
       <li>
-        <Link variant="simple-alt" href="/dashboard/logout">Log Out</Link>
+        <Link variant="simple-alt" href="/dashboard/logout">
+          <FiLogOut style={{marginRight: '16px'}}/>
+          Log out
+        </Link>
       </li>
     </>
   )
