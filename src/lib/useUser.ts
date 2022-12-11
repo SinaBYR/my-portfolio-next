2d154c4429e8acb9984a8a4d127b890267593274
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-12-11 20:42:01 +0330

    Use isLoggedIn property of returned object by api/user endpoint to redirect.

--- src/lib/useUser.ts
+++ src/lib/useUser.ts
@@ -6,17 +6,15 @@ import { fetchJson } from './fetchJson';
 
 export function useUser({
   redirectTo = ''
-}: {
-  redirectTo: string
-}) {
+} = {}) {
   const { data: user, mutate: mutateUser, error } = useSWR<User>('/api/user', fetchJson);
 
   useEffect(() => {
     // if user data not yet there (fetch in progress, logged in or not) then don't do anything yet.
     if(!user || !redirectTo) return;
 
-    // redirect user if user not found (not logged in).
-    if(!user && redirectTo) {
+    // redirect user if not logged in.
+    if(redirectTo && !user?.isLoggedIn) {
       Router.push(redirectTo);
     }
   }, [user, redirectTo]);
