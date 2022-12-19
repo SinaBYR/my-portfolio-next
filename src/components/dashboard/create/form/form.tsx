ae1cfff949c34f6759df88e730a9d33b322fa9c7
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-12-19 22:22:48 +0330

    Clean up Form component after extracting SelectRepo and TechTags components.

--- src/components/dashboard/create/form/form.tsx
+++ src/components/dashboard/create/form/form.tsx
@@ -1,12 +1,11 @@
 import classes from './form.module.scss';
 import { Button } from '../../../utilities';
 import { useFormik } from 'formik';
-import AsyncSelect from 'react-select/async';
-import { fetchJson } from '../../../../lib/fetchJson';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Editor } from '@tinymce/tinymce-react';
-import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
+import { TechTags } from './techTags/techTags';
+import { SelectRepo } from './selectRepo/selectRepo';
 
 export function Form() {
   const { values, handleChange, setFieldValue, handleSubmit } = useFormik({
@@ -22,38 +21,7 @@ export function Form() {
       console.log(values)
     }
   });
-
-  // update tech list on drop event.
-  function handleDrop(droppedItem: DropResult) {
-    if (!droppedItem.destination) return;
-
-    const updatedList = [...values.technologies];
-    const reorderedItem = updatedList.splice(droppedItem.source.index, 1)[0];
-    updatedList.splice(droppedItem.destination.index, 0, reorderedItem);
-    setFieldValue('technologies', updatedList);
-  };
-
-  // repository selection search-box
-  function filterOptions(inputValue: string, options: any[]) {
-    return options.filter(i => i.label.toLowerCase().includes(inputValue.toLowerCase()));
-  };
-
-  // fetch github repositories.
-  async function loadOptions(inputValue: string) {
-    try {
-      const repos: any[] = await fetchJson('https://api.github.com/users/sinabyr/repos?sort=created');
-      const options = repos.map(r => {
-        return {
-          value: r.name,
-          label: r.name
-        }
-      })
-      return filterOptions(inputValue, options);
-    } catch(err) {
-        toast.error(err.message);
-    }
-  };
-
+  
   return (
     <form onSubmit={handleSubmit}>
       <button>Click</button>
@@ -76,77 +44,12 @@ export function Form() {
         </div>
         <div>
           <label htmlFor="technologies">Technologies</label>
-          <input type="text" id="technologies" name="technologies" onKeyDown={e => {
-            if(!e.currentTarget.value) return;
-            if(e.key === 'Enter') {
-              const techList = [...values.technologies];
-              techList.push(e.currentTarget.value);
-              setFieldValue('technologies', techList);
-              e.currentTarget.value = '';
-              // to prevent form from submitting.
-              e.preventDefault();
-            }
-          }}/>
+          <TechTags values={values} setFieldValue={setFieldValue}/>
         </div>
         <div>
           <label htmlFor="repo">Repository</label>
-          <AsyncSelect
-            name="repo"
-            id="repo"
-            onChange={(e: any) => setFieldValue('repo', e.value)}
-            styles={{
-              singleValue: baseStyles => ({
-                ...baseStyles,
-                color: '#f4f4f4',
-              }),
-              input: baseStyles => ({
-                ...baseStyles,
-                color: '#f4f4f4',
-              }),
-              control: baseStyles => ({
-                ...baseStyles,
-                backgroundColor: '#1a1d1d',
-                paddingBottom: '3px',
-                marginBottom: '1rem',
-                border: '1px solid #020303',
-              }),
-              option: baseStyles => ({
-                ...baseStyles,
-                color: '#1a1d1d'
-              }),
-              menu: baseStyles => ({
-                ...baseStyles,
-                zIndex: 2
-              })
-            }}
-            loadOptions={loadOptions}
-            defaultOptions/>
+          <SelectRepo setFieldValue={setFieldValue}/>
         </div>
-        {!!values.technologies.length &&
-        <DragDropContext onDragEnd={handleDrop} >
-          <Droppable droppableId="tech-list-droppable" direction="horizontal">
-            {provided => (
-              <div
-                className={classes.techList}
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {values.technologies.map((t, index) => (
-                  <Draggable key={t + index} draggableId={t + index} index={index}>
-                    {provided => (
-                      <span
-                        ref={provided.innerRef}
-                        {...provided.dragHandleProps}
-                        {...provided.draggableProps}
-                      >{t}</span>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>}
         <div>
           <label htmlFor="description">Description</label>
           <Editor
