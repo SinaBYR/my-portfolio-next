ac708ac4e26c0ae3b19acce5166a2c239d3cb0f4
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-12-30 21:10:43 +0330

    Migrate to postgresql from mariadb in projects api route.

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -1,6 +1,6 @@
 import { withIronSessionApiRoute } from "iron-session/next";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { db } from "../../db/db";
+import { db } from "../../db";
 import { sessionOptions } from "../../lib/session";
 import formidable from 'formidable';
 import { IncomingMessage } from "http";
@@ -16,7 +16,7 @@ export const config = {
 
 export default withIronSessionApiRoute(handler, sessionOptions)
 
-interface ProjectType {
+interface Project {
   id: string;
   title: string;
   created_at: string;
@@ -31,6 +31,99 @@ interface FormDataInterface {
   thumbnail: string;
   technologies: string[];
   screenshots: formidable.File[];
+}
+
+async function handler( req: NextApiRequest, res: NextApiResponse ) {
+  if(!req.session.userId) {
+    return res.status(401).send({
+      message: 'Unauthorized access to resources.'
+    });
+  }
+
+  const client = await db.connect();
+
+  if(req.method === 'POST') {
+    try {
+      const {
+        title,
+        repo,
+        demo_url,
+        description,
+        thumbnail,
+        technologies,
+        screenshots
+      } = await getFormData(req) as FormDataInterface;
+  
+      if(!title || !repo || !technologies.length) {
+        return res.status(502).send({ message: 'Missing parameters' });
+      }
+  
+      let thumbnailUrl: string;
+  
+      if(screenshots.length) {
+        // upload screenshots in parallel
+        const screenshotsArray = await Promise.all(getS3UploadPromises(title, screenshots));
+        // grab the selected screenshot used as thumbnail
+        thumbnailUrl = screenshotsArray.filter(({Key}) => Key.substring(Key.lastIndexOf('/') + 1) === thumbnail)[0]?.Location;
+  
+        if(!thumbnailUrl) {
+          // even tho screenshots are provided, thumbnail value doesn't correspond
+          // to the name of one of them..
+          return res.status(502).send({ message: 'Selected screenshot image as thumbnail is invalid.' });
+        }
+      }
+  
+      const projectTableQuery = `
+        insert into project (title, repo, demo_url, description, thumbnail)
+        values ($1, $2, $3, $4, $5) returning id, title, created_at, last_edited_at;
+      `;
+  
+      const { rows } = await client.query(projectTableQuery, [
+        title,
+        repo,
+        demo_url ?? null,
+        description ?? null,
+        thumbnailUrl ?? null
+      ]);
+
+      const newProject: Project = rows[0];
+
+      const technologyTableQuery = `
+        insert into technology (p_id, name)
+        values ($1, $2);
+      `;
+
+      const technologiesPromises = technologies.map(t => {
+        return client.query(technologyTableQuery, [newProject.id, t])
+      });
+
+      await Promise.all(technologiesPromises);
+  
+      client.release();
+
+      return res.json(newProject);
+    } catch(err) {
+      return res.status(500).send(err);
+    }
+  }
+
+  const query = `
+    select id, title, created_at, last_edited_at
+    from project
+    order by created_at desc;
+  `;
+
+  try {
+    const result = await client.query(query);
+
+    const projects: Project[] = result.rows;
+
+    client.release();
+
+    res.json(projects);
+  } catch(err) {
+    res.status(500).send(err);
+  }
 }
 
 // If only one screenshot is provided, then an object will be returned, otherwise an array of objects will be returned.
@@ -72,87 +165,4 @@ function getS3UploadPromises(subDirectory: string, screenshots: any[]) {
   })
 
   return promises;
-}
-
-async function handler( req: NextApiRequest, res: NextApiResponse ) {
-  if(!req.session.userId) {
-    return res.status(401).send({
-      message: 'Unauthorized access to resources.'
-    });
-  }
-
-  const conn = await db.pool.getConnection();
-
-  if(req.method === 'POST') {
-    try {
-      const {
-        title,
-        repo,
-        demo_url,
-        description,
-        thumbnail,
-        technologies,
-        screenshots
-      } = await getFormData(req) as FormDataInterface;
-  
-      if(!title || !repo || !technologies.length) {
-        return res.status(502).send({ message: 'Missing parameters' });
-      }
-  
-      let thumbnailUrl: string;
-  
-      if(screenshots.length) {
-        // upload screenshots in parallel
-        const screenshotsArray = await Promise.all(getS3UploadPromises(title, screenshots));
-        // grab the selected screenshot used as thumbnail
-        thumbnailUrl = screenshotsArray.filter(({Key}) => Key.substring(Key.lastIndexOf('/') + 1) === thumbnail)[0]?.Location;
-  
-        if(!thumbnailUrl) {
-          // even tho screenshots are provided, thumbnail value doesn't correspond
-          // to the name of one of them..
-          return res.status(502).send({ message: 'Selected screenshot image as thumbnail is invalid.' });
-        }
-      }
-  
-      const projectTableQuery = `
-        insert into project (title, repo, demo_url, description, thumbnail)
-        values (?, ?, ?, ?, ?) returning id, title, created_at, last_edited_at;
-      `;
-  
-      const [newProject]: ProjectType[] = await conn.query(projectTableQuery, [
-        title,
-        repo,
-        demo_url ?? null,
-        description ?? null,
-        thumbnailUrl ?? null
-      ]);
-
-      const technologyTableQuery = `
-        insert into technology (p_id, name) values (?, ?);
-      `;
-      const promises = technologies.map(t => {
-        return conn.query(technologyTableQuery, [newProject.id, t])
-      });
-      await Promise.all(promises);
-  
-      conn.release();
-      return res.json(newProject);
-    } catch(err) {
-      return res.status(500).send(err);
-    }
-  }
-
-  const query = `
-    select id, title, created_at, last_edited_at
-    from project
-    order by created_at desc;
-  `;
-
-  try {
-    const projects: ProjectType[] = await conn.query(query);
-    conn.release();
-    res.json(projects);
-  } catch(err) {
-    res.status(500).send(err);
-  }
 }
